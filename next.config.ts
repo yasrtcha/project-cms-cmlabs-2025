0196e49de000ca101ef42da4e8cf53017f57f886
Rais <danarraisa@gmail.com>
2026-02-18 16:58:37 +0700

    nonaktifkan eslint saat build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   // Image optimization
   images: {
     // --- TAMBAHAN PENTING DI SINI ---
